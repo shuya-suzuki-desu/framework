860af6a11632c8cbf2768270ac864debd9683434
geneic-niue <s.suzuki@vrst.co.jp>
2023-07-03 17:16:52 +0900

    システム内エラー作成 IllegalCommandException

---
+++ src/main/java/jp/co/vrst/exception/BaseException.java
@@ -0,0 +1,11 @@
+package jp.co.vrst.exception;
+
+public class BaseException extends RuntimeException {
+
+    public BaseException() {
+        super();
+    }
+    public BaseException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/jp/co/vrst/exception/IllegalCommandException.java
@@ -0,0 +1,11 @@
+package jp.co.vrst.exception;
+
+public class IllegalCommandException extends BaseException {
+    public IllegalCommandException() {
+        super();
+    }
+
+    public IllegalCommandException(String message) {
+        super(message);
+    }
+}
